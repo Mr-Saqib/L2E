2f1280ab1cc2ed1c78080bc355c75f75b4e02481
Sarmad-Rj <sarmadrajpoot291@gmail.com>
2024-05-07 19:39:12 +0500

    something

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -6,6 +6,8 @@ import { ApiService } from 'src/app/core/api.service';
 import { GlobalService } from 'src/app/core/global.service';
 import { ToastService } from 'src/app/core/toasts/toast.service';
 import { set } from '../../localStorage/LocalStorage';
+import { AlertController, ToastController } from '@ionic/angular';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -24,7 +26,9 @@ export class LoginPage implements OnInit {
     public alert: AlertService,
     public toast: ToastService,
     public ApiCall: ApiService,
-    public global: GlobalService
+    public global: GlobalService,
+    public alertController: AlertController,
+    public toastController: ToastController
   ) {
     this.menu.enable(false);
   }
@@ -32,15 +36,80 @@ export class LoginPage implements OnInit {
     this.menu.enable(false);
   }
 
-  async login() {
-    console.log(this.login_data)
+  async loginfirst() {
+    console.log(this.login_data);
     await this.ApiCall.user_Login(this.login_data);
     this.global.Userlogin.subscribe((res) => {
       set('user', res?.user);
       this.login_data = res;
     });
   }
+  public username: any;
+  public password: any;
+  incorrectAttempts: number = 0;
+  showCountdown: boolean = false;
+  countdown: number = 30;
+  async login() {
+    console.log('userName: ', this.username);
+    console.log('password: ', this.password);
+    if (this.username === 'admin') {
+      if (this.password === '12345') {
+        this.route.navigate(['home']);
+      } else {
+        this.incorrectAttempts++;
+        if (this.incorrectAttempts >= 3) {
+          this.showCountdown = true;
+          this.countdownTimer();
+          this.incorrectAttempts = 0; // Reset attempts
+          const alert = await this.alertController.create({
+            // header: 'Error',
+            message: `Too many wrong attempts!! Please wait for ${this.countdown}s`,
+            // cssClass: 'alrt',
+            buttons: ['OK'],
+          });
+          alert.present();
+        } else if (this.incorrectAttempts < 3) {
+          const alert = await this.alertController.create({
+            header: 'Error',
+            message: 'Your password is wrong!!',
+            // cssClass: 'alrt',
+            buttons: ['OK'],
+          });
+          alert.present();
+        } else {
+          // Move the button away on wrong credentials
+        }
+      }
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Username is not available.',
+        // cssClass: 'alrt',
+        buttons: ['OK'],
+      });
+      alert.present();
+    }
+  }
+  passwordType: string = 'password';
+  passwordIcon: string = 'eye-off';
+  togglePasswordVisibility() {
+    this.passwordType = this.passwordType === 'password' ? 'text' : 'password';
+    this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
+  }
+  countdownTimer() {
+    const interval = setInterval(() => {
+      this.countdown--;
+      if (this.countdown === 0) {
+        clearInterval(interval);
+        this.resetCountdown();
+      }
+    }, 1000);
+  }
 
+  resetCountdown() {
+    this.showCountdown = false;
+    this.countdown = 30;
+  }
   disableIonSplitPange() {
     this.ionSplitPane.disabled = true;
   }
